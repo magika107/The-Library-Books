7b477565d8291bf0a55e4a589ffb3cc9d41704b8
Mehdi.ghasemi <mehdighasemi10710@gmail.com>
2025-06-12 16:07:15 +0330

    The Library Books

---
+++ src/main/java/model/entities/Account.java
@@ -0,0 +1,9 @@
+package model.entities;
+
+public class Account {
+    private int id;
+    private Person person;
+    private int amount;
+    private TransactionType transactionType;
+
+}
---
+++ src/main/java/model/entities/Person.java
@@ -0,0 +1,15 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Person {
+    private int id;
+    private String name;
+    private String family;
+    private String username;
+    private String password;
+    private LocalDate birthDate;
+    private String phonenumber;
+
+}
